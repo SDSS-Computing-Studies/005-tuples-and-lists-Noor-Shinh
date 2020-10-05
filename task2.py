813a8fb2f912d3280cc8bbc95268897cdd25452b
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-05 12:59:16 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -23,10 +23,10 @@ Enter a word: virus
 
 ['apple', 'worm', 'dollar', 'shingle', 'virus']
 """
-word1=input("Enter a word")
-word2=input("Enter a word")
-word3=input("Enter a word")
-word4=input("Enter a word")
-word5=input("Enter a word")
+word1=(input("Enter a word")).strip()
+word2=(input("Enter a word")).strip()
+word3=(input("Enter a word")).strip()
+word4=(input("Enter a word")).strip()
+word5=(input("Enter a word")).strip()
 words=[word1,word2,word3,word4,word5]
 print(words)
